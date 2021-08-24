6f94d1533385f81c4fc0b9bbd5ba3bb2a17d39ca
Nathan Vontz <nvontz@ksu.edu>
2021-08-24 17:07:00 -0500

    fixed build issues

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,20 @@
+import { fromPairs } from "lodash";
+import React, { Component } from 'react';
+
+class ErrorBoundry extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasError: false
+        }
+    }
+
+    render() {
+        if(this.state.hasError) {
+            return <h1>Ooops. That is not good</h1>
+        }
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundry;
